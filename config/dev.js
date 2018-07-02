49148f6e4db795b1779f9325dc513929fb178415
Kevin Amiranoff <kamiranoff@Lucass-iPhone.home>
2018-07-03 00:24:07 +0100

    initial commit - set up starter

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '539716729018-udpg9ctfs9snal2pu52ucmnhtu5dlfao.apps.googleusercontent.com',
+  googleClientSecret: 'boXmmmRJkOzqVmrrNXKjecbw',
+  mongoURI: 'mongodb://localhost:27017/blog_dev',
+  cookieKey: '123123123'
+};
